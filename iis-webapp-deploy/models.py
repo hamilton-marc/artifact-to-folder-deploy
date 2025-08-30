4d3b41ebb9e0b854097c0d693d193a7de8b10bc4
Marc Hamilton <unionhills@cox.net>
2025-08-30 10:02:26 -0700

    Creating models for the YAML configuration file

--- iis-webapp-deploy/models.py
+++ iis-webapp-deploy/models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel, Field, ValidationError
 
 class AppParams(BaseModel):
@@ -8,4 +10,38 @@ class AppParams(BaseModel):
     app: str = Field(description="Application to deploy")
     env: str = Field(description="The target environment to deploy to")
     run_id: str = Field(default="latest", description="Run id from GithHub Actions to Deploy")
+
+
+class GitHubConfig(BaseModel):
+    """
+    Base configuration settings for the GitHub API
+    """
+
+    base_url: str
+    token: str
+    owner: str
+
+
+class ProjectConfig(BaseModel):
+    """
+    Base configuration settings for the project
+    """
+
+    name: str
+    repo: str
+    workflow_filename: str
+    download_directory: str
+    websites: List[str]
+    allowed_branches: List[str]
+
+
+class AppConfig(BaseModel):
+    """
+    Validated parameters for the application configuration
+    """
+
+    websites_base_path: str
+    temp_extract_path: str
+    github_config: GitHubConfig
+    projects: List[ProjectConfig]
 
